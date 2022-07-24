9daf8e0577d61946bd0c682589280ac575dd5484
John Wiegley <johnw@newartisans.com>
2022-07-23 22:25:30 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 args@{
   rev    ? "e0a42267f73ea52adc061a64650fddc59906fc99"
 , sha256 ? "0r1dsj51x2rm016xwvdnkm94v517jb1rpn4rk63k6krc4d0n3kh9"
+
 , pkgs   ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
@@ -11,7 +12,8 @@ args@{
 
 let
 
-software-foundations = coqPackages: with pkgs.${coqPackages}; pkgs.stdenv.mkDerivation rec {
+software-foundations = coqPackages:
+  with pkgs.${coqPackages}; pkgs.stdenv.mkDerivation rec {
   name = "coq${coq.coq-version}-software-foundations-${version}";
   version = "1.0";
 
@@ -24,10 +26,6 @@ software-foundations = coqPackages: with pkgs.${coqPackages}; pkgs.stdenv.mkDeri
   ];
   enableParallelBuilding = true;
 
-  buildFlags = [
-    "JOBS=$(NIX_BUILD_CORES)"
-  ];
-
   installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
 
   env = pkgs.buildEnv { inherit name; paths = buildInputs; };
@@ -37,6 +35,7 @@ software-foundations = coqPackages: with pkgs.${coqPackages}; pkgs.stdenv.mkDeri
 };
 
 in {
+  inherit software-foundations;
   software-foundations_8_14 = software-foundations "coqPackages_8_14";
   software-foundations_8_15 = software-foundations "coqPackages_8_15";
 }
